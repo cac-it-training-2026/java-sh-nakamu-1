e8d99403adc7ca4bbbf74843be3d506acc8f1907
shnakamu <shnakamu@C010423453.zipangu.cac.co.jp>
2026-04-16 12:57:27 +0900

    question6まで完了

--- java_comprehension_exercises_volume1/src/basic/question02/Question02_2.java
+++ java_comprehension_exercises_volume1/src/basic/question02/Question02_2.java
@@ -1,6 +1,8 @@
 package basic.question02;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * 第5章 入出力
@@ -10,7 +12,16 @@ public class Question02_2 {
 
 	public static void main(String[] args) throws IOException {
 
-		// TODO:ここに実装
+		System.out.println("商品の名前と値段を入力してください。");
+
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		String itemName = reader.readLine();
+
+		String itemPriceString = reader.readLine();
+		int itemPrice = Integer.parseInt(itemPriceString);
+
+		System.out.println("商品の名前は" + itemName + "です。");
+		System.out.println("商品の値段は" + itemPrice + "円です。");
 
 	}
 }
