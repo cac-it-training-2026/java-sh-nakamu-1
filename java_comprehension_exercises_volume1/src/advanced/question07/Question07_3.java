7f0d912ba89ba5955628fba74b72bcccee0e2e1f
shnakamu <shnakamu@C010423453.zipangu.cac.co.jp>
2026-04-16 14:27:58 +0900

    question07_3の途中

--- java_comprehension_exercises_volume1/src/advanced/question07/Question07_3.java
+++ java_comprehension_exercises_volume1/src/advanced/question07/Question07_3.java
@@ -1,6 +1,8 @@
 package advanced.question07;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Question07_3 {
 
@@ -13,8 +15,40 @@ public class Question07_3 {
 		// 購入数（変更不可）
 		int[] amounts = new int[5];
 
-		// TODO: 実装ここから
+		int[] stocks = new int[itemNames.length];
 
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+		for (int i = 0; i < itemNames.length; i++) {
+			System.out.println(itemNames[i] + "の在庫数＞");
+			String stockString = reader.readLine();
+			stocks[i] = Integer.parseInt(stockString);
+		}
+
+		for (int i = 0; i < itemNames.length; i++) {
+			int amount = 0;
+			boolean isValid = false;
+
+			while (isValid == false) {
+				System.out.println(itemNames[i] + "の購入数＞");
+				String amountString = reader.readLine();
+				amount = Integer.parseInt(amountString);
+
+				if (amount > stocks[i]) {
+					System.out.println("在庫を超えています。もう一度入力してください。");
+					continue;
+				} else {
+					amounts[i] = amount;
+					isValid = true;
+				}
+			}
+		}
+
+		for (int i = 0; i < itemNames.length; i++) {
+			System.out.println(itemNames[i] + "：" + "購入" + amounts[i] + "個");
+			stocks[i] = stocks[i] - amounts[i];
+			System.out.println("在庫" + stocks[i] + "個");
+		}
 
 	}
 }
